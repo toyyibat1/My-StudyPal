b27af715b63e931998e8ccfe877364de25ab0d3e
unknown <toyyibat.abdulkareem@gmail.com>
2020-11-30 14:33:08 +0100

    created splash screen, onboarding and signup screen

---
+++ lib/src/core/constants.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+/// App Colors
+const kPrimaryColor = Color(0xFF225ADC);
+
+const kSecondaryColor = Color(0x36C4C4C4);
+
+const kErrorColor = Colors.red;
+
+const kHintColor = Color(0xFF6F6565);
+
+const kTextFieldFillColor = Color(0x80E5E5E5);
+
---
+++ lib/src/core/images.dart
@@ -0,0 +1,11 @@
+
+const logo = 'assets/logo.png';
+const logo2 = 'assets/logo2.png';
+const onboarding1 = 'assets/onboarding1.png';
+const onboarding2 = 'assets/onboarding2.png';
+const onboarding3 = 'assets/onboarding3.png';
+const onboarding4 = 'assets/onboarding4.png';
+
+const facebook = 'assets/facebook.png';
+const google = 'assets/google.png';
+const email = 'assets/emailbox.png';
