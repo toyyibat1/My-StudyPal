b20ae43ae6ec6b95b76d7f66d3796faaabcce648
Mubarak Shuaib <mubarakshuaib3@gmail.com>
2021-02-07 01:47:37 +0100

    update

--- lib/src/views/screens/badges_screen.dart
+++ lib/src/views/screens/badges_screen.dart
@@ -2,12 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
 import 'package:my_study_pal/src/controller/badges_controller.dart';
-import 'package:my_study_pal/src/controller/dashboard_controller.dart';
 import 'package:my_study_pal/src/core/constants.dart';
 import 'package:my_study_pal/src/core/images.dart';
 import 'package:my_study_pal/src/core/notifier.dart';
 import 'package:my_study_pal/src/views/widgets/badge_tile.dart';
-import '../../models/badges.dart';
 
 class BadgesScreen extends StatelessWidget {
   // final List<TaskBadges> badges;
@@ -16,104 +14,107 @@ class BadgesScreen extends StatelessWidget {
   // @override
 
   Widget build(BuildContext context) {
-    return  GetBuilder<BadgeController>(
-      init: BadgeController()..getAuthenticatedUser(),
-      builder: (controller) =>Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            header(context),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  kMediumVerticalSpacing,
-                  Text(
-                    'Earn Badges',
-                    style: kLabelText,
-                  ),
-                  Expanded(
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 0),
-                child: controller.state == NotifierState.isLoading
-                    ? Center(child: CircularProgressIndicator())
-                    : controller.badges.isEmpty
-                        ? Center(
-                            child: SizedBox(
-                              height: 180,
-                              child: Column(
-                                crossAxisAlignment:
-                                    CrossAxisAlignment.stretch,
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  SvgPicture.asset(
-                                    'assets/no_task.svg',
-                                    color: Colors.grey.shade300,
-                                  ),
-                                  SizedBox(height: 8.0),
-                                  Center(
-                                    child: Text(
-                                      'No Badges',
-                                      style: TextStyle(
-                                        color: Colors.grey,
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
+    return GetBuilder<BadgeController>(
+        init: BadgeController()..getAuthenticatedUser(),
+        builder: (controller) => Scaffold(
+              body: SafeArea(
+                child: Column(
+                  children: [
+                    header(context),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          kMediumVerticalSpacing,
+                          Padding(
+                            padding: const EdgeInsets.only(left: 24.0),
+                            child: Text(
+                              'Earn Badges',
+                              style: kLabelText,
                             ),
-                          )
-                        : ListView.builder(
-                            itemCount: controller.badges.length,
-                            itemBuilder: (context, index) {
-                              return BadgeTile(
-                                index: index,
-                                badges: controller.badges,
-                                controller: controller,
-                              );
-                            },
                           ),
+                          Expanded(
+                            child: Padding(
+                              padding:
+                                  const EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 0),
+                              child: controller.state == NotifierState.isLoading
+                                  ? Center(child: CircularProgressIndicator())
+                                  : controller.badges.isEmpty
+                                      ? Center(
+                                          child: SizedBox(
+                                            height: 180,
+                                            child: Column(
+                                              crossAxisAlignment:
+                                                  CrossAxisAlignment.stretch,
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment.center,
+                                              children: [
+                                                SvgPicture.asset(
+                                                  'assets/no_task.svg',
+                                                  color: Colors.grey.shade300,
+                                                ),
+                                                SizedBox(height: 8.0),
+                                                Center(
+                                                  child: Text(
+                                                    'No Badges',
+                                                    style: TextStyle(
+                                                      color: Colors.grey,
+                                                    ),
+                                                  ),
+                                                ),
+                                              ],
+                                            ),
+                                          ),
+                                        )
+                                      : ListView.builder(
+                                          itemCount: controller.badges.length,
+                                          itemBuilder: (context, index) {
+                                            return BadgeTile(
+                                              index: index,
+                                              badges: controller.badges,
+                                              controller: controller,
+                                            );
+                                          },
+                                        ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
-              ),
-            ],
-              ),
-            ),
-          ],
-        ),
-      ),
-    )
-    );
+            ));
   }
 
   Widget taskBadges(BadgeController badgeController) {
     return Row(
+      children: [
+        Expanded(
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 32),
+            margin: EdgeInsets.symmetric(vertical: 6),
+            decoration: BoxDecoration(
+                color: kSecondaryColor,
+                borderRadius: BorderRadius.circular(12)),
+            child: Row(
               children: [
-                Expanded(
-                  child: Container(
-                    padding: EdgeInsets.symmetric(
-                        horizontal: 16, vertical: 32),
-                    margin: EdgeInsets.symmetric(vertical: 6),
-                    decoration: BoxDecoration(
-                        color: kSecondaryColor,
-                        borderRadius: BorderRadius.circular(12)),
-                    child: Row(
-                      children: [
-                        Row(
-                          children: [
-                            Image.asset(badge),
-                            Text(
-                              '${badgeController.completedTaskBadges.length}',
-                              style: kBodyText2TextStyle,
-                            ), 
-                           // Text('${badgeController.de}')
-                          ],
-                        ),
-                      ],
+                Row(
+                  children: [
+                    Image.asset(badge),
+                    Text(
+                      '${badgeController.completedTaskBadges.length}',
+                      style: kBodyText2TextStyle,
                     ),
-                  ),
+                    // Text('${badgeController.de}')
+                  ],
                 ),
               ],
-            );
+            ),
+          ),
+        ),
+      ],
+    );
   }
 
   Widget header(BuildContext context) => Container(
--- lib/src/views/widgets/badge_tile.dart
+++ lib/src/views/widgets/badge_tile.dart
@@ -1,88 +1,67 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:my_study_pal/src/controller/badges_controller.dart';
+import 'package:my_study_pal/src/core/constants.dart';
 import 'package:my_study_pal/src/models/badges.dart';
-
-import '../../controller/timetable_controller.dart';
-import '../../core/constants.dart';
-import '../../core/images.dart';
-import '../../models/timetable.dart';
-
-import 'package:flutter/cupertino.dart';
-import 'package:my_study_pal/src/controller/timetable_controller.dart';
 
 class BadgeTile extends StatelessWidget {
   final int index;
   final List<Badge> badges;
   final BadgeController controller;
 
-  const BadgeTile (
-      {Key key, this.controller, this.badges, this.index})
+  const BadgeTile({Key key, this.controller, this.badges, this.index})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 8.0),
       child: SizedBox(
         height: 100,
         child: ClipRRect(
           borderRadius: BorderRadius.circular(20),
-          child: Row(
-            children: [
-              // Container(
-              //   width: 18,
-              //  // color: kPrimaryColor,
-              //   child: Image.asset("assets/badge.png"),
-              // ),
-              Expanded(
-                child: Container(
-                  color: Color(0xFFF4F4F4),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 8.0,
-                    ),
-                    child: Row(
-                      //mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Container(
-                              width: MediaQuery.of(context).size.width / 1.4,
-                              child: Text(
-                                  badges[index].badgeTitle,
-                                  style: TextStyle(
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.w600,
-                                  ),
-                                  overflow: TextOverflow.ellipsis,
-                                ),
-                            ),
-                            SizedBox(height: 16),
-                            Row(
-                              children: [
-                               // SizedBox(width: 6),
-                                Text(
-                                   badges[index].desc,
-                                 style: TextStyle(
-                                    fontSize: 11,
-                                    fontWeight: FontWeight.w600,
-                                  ),
-                                  overflow: TextOverflow.ellipsis,
-                                ),
-                               // SizedBox(width: 12),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
+          child: Container(
+            color: Color(0xFFF4F4F4),
+            child: Row(
+              children: [
+                CircleAvatar(
+                  backgroundColor: Colors.white,
+                  radius: 35,
+                  child: Image.asset(
+                    "assets/badge.PNG",
+                    width: 36,
                   ),
                 ),
-              ),
-            ],
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 8.0,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        badges[index].badgeTitle,
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                        ),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      SizedBox(height: 16),
+                      Container(
+                        width: MediaQuery.of(context).size.width / 1.6,
+                        child: Text(
+                          badges[index].desc,
+                          style: kLabelText,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
